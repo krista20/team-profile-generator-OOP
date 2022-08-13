bfbbf05aed36dc2afe4ec0d75ffdabb82c514bc9
Rush <kriscookies20@gmail.com>
2022-08-13 16:48:57 -0700

    added in the beginning of user input questions, & got node indes.js to not return any errors

--- index.js
+++ index.js
@@ -1,3 +1,49 @@
+const inquirer = require('inquirer');
 const Employee = require('./lib/Employee');
+const Engineer = require('./lib/Engineer.js');
+const Manager = require('./lib/Manager.js');
+const Intern = require('./lib/Intern.js');
+const fs = require("fs");
+const path = require("path");
+const dirpath = path.join(__dirname, '');
+const output = path.join(dirpath, 'index.html');
+const allEmplyees = [];
 
-new Employee().initializeEmployee();
+const inputEmployee = () => {
+    return inquirer.prompt(
+        [
+            {
+                type:'input',
+                name:'name',
+                message: 'Type in your name.'
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: 'Please type in your id number.'
+                
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'Please type in your email address.'
+            }
+    ]).then(inputs => {
+        // getting all answers for employee and pushing it to employee (starting w/ this since employee is the parent)
+        const employee = new Employee(inputs.name, inputs.id, inputs.email);
+        allEmplyees.push(employee);
+        // need function
+    })
+};
+
+const getTypeEmployee = () => {
+    return inquirer.prompt ([
+        {
+            type: 'list',
+            name: 'menu',
+            message: 'Select a type of Employee:',
+            choices: ['Manager', 'Engineer', 'Intern']
+        }
+    ])
+}
+// new Employee().initializeEmployee();
