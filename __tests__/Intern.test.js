35dc144e13b63fb09a460306b7955b58a25257cb
Rush <kriscookies20@gmail.com>
2022-08-13 13:41:44 -0700

    intern passed test

---
+++ __tests__/Intern.test.js
@@ -0,0 +1,25 @@
+// const { test } = require('picomatch');
+const { expect } = require('expect');
+const Intern = require('../lib/Intern.js');
+const Employee = require('../lib/Employee.js');
+
+const intern = new Intern('John', '5', 'john@fakemail.com', '2University');
+
+test('test for an intern employee object', () => {
+    expect(intern.name).toBe('John');
+    expect(intern.id).toBe('5');
+    expect(intern.email).toBe('john@fakemail.com');
+  
+});
+
+
+test('test for getSchool()', () => {
+    expect(intern.getSchool()).toBe('2University');
+    console.log(intern.getSchool());
+});
+
+test('test fot getRole()', () => {
+    expect(intern.getRole()).toBe("Intern");
+    console.log(intern.getRole());
+});
+
