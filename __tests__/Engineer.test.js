d5d1173eefc27277e096ce660c3353a92ac66766
Rush <kriscookies20@gmail.com>
2022-08-12 17:51:22 -0700

    currently working on engineer.test.js

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,11 +1,22 @@
 const { test } = require('picomatch');
-const Engineer = require('../lib/Engineer');
-const Employee = require('../lib/Employee');
-const engineer = new Engineer('');
+const Engineer = require('../lib/Engineer.js');
+// const Employee = require('../lib/Employee.js');
+const engineer = new Engineer('Alec', '2', 'alec@fakemail.com');
 
-test('creates an engineer employee', () => {
-    
+test('test for an engineer employee object', () => {
+    expect(engineer.name).toBe('Alec');
+    expect(engineer.id).toBe('2');
+    expect(engineer.email).toBe('alec@fakemail.com');
+    console.log(expect(engineer.email));
+});
 
+test('test for getGithub()', () => {
+    expect(engineer.getGitHub()).toBe('ibalec');
+    console.log(engineer.getGitHub());
+});
 
-})
+test('test fot getRole()', () => {
+    expect(engineer.getRole()).toBe("Employee");
+    console.log(engineer.getRole());
+});
 
